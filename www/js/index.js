e5d8f656405237ca2429ae9400a7caa5fc17ec87
Abdo Shajadi <abd.shajadi@gmail.com>
2018-02-21 16:23:19 +0200

    add markers to map

--- www/js/index.js
+++ www/js/index.js
@@ -215,6 +215,35 @@ function fetchIntel() {
 			showIntel.innerHTML = content;
 		}
 	});
+	snapshot.forEach(function(data){
+	var val = data.val();
+	locations.push([
+		val.userIntel,
+		val.lat,
+		val.lng
+	]);
+});
+
+showIntel.innerHTML = content;
+
+}
+});
+var infowindow = new google.maps.InfoWindow;
+var marker, i;
+
+for (i = 0; i < locations.length; i++) { 
+marker = new google.maps.Marker({
+ position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+ map: map
+});
+
+google.maps.event.addListener(marker, 'click', (function(marker, i) {
+ return function() {
+		 infowindow.setContent(locations[i][0]);
+		 infowindow.open(map, marker);
+ }
+})(marker, i));
+}
 }
 
 app.initialize();
